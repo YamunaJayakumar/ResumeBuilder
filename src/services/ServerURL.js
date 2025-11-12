2668efe9607f4c63112c100062499be7343565fb
YamunaJayakumar <yamunajayakumar789@gmail.com>
2025-11-12 21:00:35 +0530

    commit'

--- src/services/ServerURL.js
+++ src/services/ServerURL.js
@@ -1,5 +1,5 @@
 // http://localhost:3000 after it no slash
 //to use it anywhere export it
-const ServerURL="http://localhost:3000"
-
+// const ServerURL="http://localhost:3000"
+const ServerURL="https://resumebuilder-server-a32v.onrender.com"
 export default ServerURL
